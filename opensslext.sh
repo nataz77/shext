c7db1fcc86fa88c38bcd32dfcc6a10ebdf95a705
Simone Natalini <natalinis@outlook.com>
2022-01-26 12:37:22 +0100

    Fix usage fn name

--- opensslext.sh
+++ opensslext.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function usage {
+function makepfx-usage {
   echo 'makepfx: usage'
   echo 'Eg: makepfx cert.pfx key.pem cert.pem'
   echo 'makepfx {outcert} {inkey} {in-cert}'
@@ -9,7 +9,7 @@ function usage {
 function makepfx {
    if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] 
    then
-      usage
+      makepfx-usage
    else
         openssl pkcs12 -export -out "$1" -inkey "$2" -in "$3"
    fi
